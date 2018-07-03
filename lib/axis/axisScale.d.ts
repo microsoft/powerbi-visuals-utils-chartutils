beb37ac084d32993fba4066a319d13dd80b2c7fa
Ilfat Galiev <ilfat.galiev@akvelon.com>
2018-07-03 11:46:32 +0300

    Remove lib folder

--- lib/axis/axisScale.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const linear: string;
-export declare const log: string;
--- lib/legend/legendData.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbi from "powerbi-visuals-tools";
-import { LegendData } from "./legendInterfaces";
-export declare const DefaultLegendLabelFillColor: string;
-export declare function update(legendData: LegendData, legendObject: powerbi.DataViewObject): void;
